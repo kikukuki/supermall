50b5de96d8994aed2cfe913f1750b86e123cc64f
John Doe <891787859@qq.com>
2020-04-27 14:55:08 +0800

    初始化项目

---
+++ src/components/common/tabbar/TabBarItem.vue
@@ -0,0 +1,17 @@
+<template>
+    <div id="tab-bar-item">
+        <div class="item-icon" v-show="!isActive"><slot name='icon'></slot></div>
+        <div class="item-active-icon" v-show="!isActive"><slot name='active-icon'></slot></div>
+        <div class="item-text" v-show="!isActive"><slot name='text'></slot></div>
+    </div>
+</template>
+<script>
+export default {
+    name="TabBarItem",
+    computed: {
+        isActive(){
+            return this.$route.path.indexOf(this.link) !== -1
+        }
+    },
+}
+</script>
---
+++ src/router/index.js
@@ -0,0 +1,13 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+//1、安装插件
+Vue.use(VueRouter)
+
+//2、创建router 
+const routers=[]
+const router=new VueRouter({
+    routers,
+    mode:'history'
+})
+
+export default router
